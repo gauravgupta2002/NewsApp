e36e4eb47b416ca8b53aefbb0e788d783bf4d2d3
gauravgupta2002 <gauravgupta200222@gmail.com>
2021-09-07 17:22:10 +0530

    Rest Files

--- app/src/main/java/com/example/newsapp/CustomAdapter.java
+++ app/src/main/java/com/example/newsapp/CustomAdapter.java
@@ -2,6 +2,7 @@ package com.example.newsapp;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -15,10 +16,12 @@ import java.util.List;
 public class CustomAdapter extends RecyclerView.Adapter<CustomViewHolder> {
     private Context context;
     private List<NewsHeadlines> headlines;
+    private SelectListner listner;
 
-    public CustomAdapter(Context context, List<NewsHeadlines> headlines) {
+    public CustomAdapter(Context context, List<NewsHeadlines> headlines, SelectListner listner) {
         this.context = context;
         this.headlines = headlines;
+        this.listner = listner;
     }
 
     @NonNull
@@ -35,6 +38,13 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomViewHolder> {
         if (headlines.get(position).getUrlToImage()!=null){
             Picasso.get().load(headlines.get(position).getUrlToImage()).into(holder.img_headline);
         }
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listner.OnNewsClicked(headlines.get(position));
+            }
+        });
 
     }
 
